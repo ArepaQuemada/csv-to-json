dc980532b49bcad7ebfacb01ef056c2dafdfb1d5
Juan <rigualjuan@gmail.com>
2023-02-07 21:39:14 -0300

    add todos, create readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# CSV TO JSON
+
+Generates a json file from a csv file
+
+Work in progress
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const pipe = require("./utils/pipe");
 const arrayToObject = require("./utils/array-to-object");
 const makeJson = require("./utils/make-json");
 
+/**
+ * TODO
+ * Implementar un backend que reciba el csv y devuelva el formato JSON
+ */
+
 const readCSV = (path = "./username.csv") => {
   const csvFile = fs.readFileSync(path, "utf-8");
   const jsonString = pipe(convertFileToArray, arrayToObject, makeJson)(csvFile);
